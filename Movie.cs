35aa21ac05ba74b13954a68a01824e17bc1e51ae
Cameron Peterson <cam.peterson@protonmail.com>
2023-02-15 21:31:43 -0600

    Approaching complete.
    Use less try catch where possible. Implemented menu, as well as somewhat flawed write menus.

--- Movie.cs
+++ Movie.cs
@@ -1,7 +1,48 @@
-namespace MovieLibrary; 
+using System.Text;
 
-public record Movie(
+namespace MovieLibrary;
+
+public sealed record Movie(
     int Id,
     string Name,
     string[] Genres
-);
+) {
+    private string GenreString() {
+        StringBuilder sb = new();
+
+        foreach(string genre in Genres) {
+            sb.Append($"{genre}|");
+        }
+
+        sb.Length--;
+
+        return sb.ToString();
+    }
+
+    public bool Equals(Movie? other) {
+        return
+            other is not null &&
+            this.GetHashCode() == other.GetHashCode(); //Compare hashes.
+    }
+
+    public override int GetHashCode() { //Get a hash of the record, based on only the name and string array.
+        HashCode hc = new();
+        hc.Add(Name);
+
+        foreach(string genre in Genres) {
+            hc.Add(genre);
+        }
+
+        return hc.ToHashCode();
+    }
+
+    public override string ToString() {
+        return Name.Contains(',') ?
+            $"{Id},\"{Name}\",{GenreString()}" :
+            $"{Id},{Name},{GenreString()}";
+    }
+
+    public string UserToString() {
+        return $"ID: {Id}, \"{Name}\", {string.Join(", ", Genres)}";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 using NLog;
 
 namespace MovieLibrary;
@@ -10,6 +11,8 @@ static class Program {
     private static readonly string MoviesFull = Directory.GetCurrentDirectory() + '/' + Movies;
     private static readonly Regex Regex = new("""(?!\B"[^"]*),(?![^"] * "\B)""");
     private static Logger _logger = null!;
+    
+    //TODO, Cache read results, and only read when user asks.
 
     private static void Main(string[] args) {
         if(File.Exists(NLogConfFull)) {
@@ -20,18 +23,67 @@ static class Program {
             _logger.Error($"NLog config file does not exist at expected directory {NLogConfFull}");
         }
 
-        Read(out Movie[] test);
+        bool run = true;
+        while(run) {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. (R)ead from file");
+            Console.WriteLine("2. (W)rite to file");
+            Console.WriteLine("3. (E)xit");
+            
+            switch(Console.ReadLine()?[0]) {
+                case '1':
+                case 'r':
+                case 'R':
+                    if(Read(out Movie[] movies))
+                        foreach(var movie in movies)
+                            Console.WriteLine(movie.UserToString());
+                    else
+                        _logger.Error("Read failed... See above for reason.");
+                    break;
+                case '2':
+                case 'w':
+                case 'W':
+                    if(!CreateRecords(out Movie[] moviesToAdd)) {
+                        _logger.Error("Failed to create records.");
+                        break;
+                    }
+
+                    if(!WriteAppend(moviesToAdd)) {
+                        _logger.Error("Failed to append records to database.");
+                        break;
+                    }
+                    
+                    Console.WriteLine("Added the following movies:");
+                    foreach(var movie in moviesToAdd)
+                        Console.WriteLine($" {movie.UserToString()}");
+                    
+                    break;
+                case '3':
+                case 'e':
+                case 'E':
+                    run = false;
+                    break;
+                default:
+                    _logger.Warn("Invalid user input supplied. (Not 123rweRWE)");
+                    break;
+            }
+        }
     }
 
     private static bool Read(out Movie[] movies) {
+        Stopwatch sw = new();
+        FileStream fs;
         StreamReader sr;
+
         List<Movie> movieList = new();
 
-        try {
-            FileStream fs = new(MoviesFull, FileMode.Open);
+        sw.Start();
+
+        if(File.Exists(MoviesFull)) {
+            fs = new(MoviesFull, FileMode.Open);
             sr = new(fs);
-        } catch(Exception e) {
-            _logger.Warn(e.Message);
+        } else {
+            _logger.Warn("File");
             movies = Array.Empty<Movie>();
             return false;
         }
@@ -43,28 +95,116 @@ static class Program {
             lineCount++;
 
             if(line is null) {
-                _logger.Warn($"Null line at line {lineCount}");
+                _logger.Warn("Null line at line {lineNum}", lineCount);
                 continue;
             }
 
             string[] split = Regex.Split(line);
 
             if(int.TryParse(split[0], out int movieId)) {
-                movieList.Add(new Movie(movieId,
-                    split[1].Replace("\"",
-                        ""), split[2].Split('|')));
-                _logger.Info("Found movie {Movie}", movieList.Last().Name);
+                Movie toAdd = new(
+                    movieId,
+                    split[1].Replace("\"", ""),
+                    split[2].Split('|'));
+
+                if(movieList.Contains(toAdd)) {
+                    _logger.Warn("Found duplicate of movie {Movie} at {lineNum}", toAdd.Name, lineCount);
+                } else {
+                    movieList.Add(toAdd);
+                    _logger.Info("Found movie {Movie}", toAdd.Name);
+                }
             } else {
                 _logger.Error("Non-Integer Movie ID, Skipping!");
                 //continue;
             }
         }
 
+        sw.Stop();
+
+        _logger.Debug("Movie parsing took {Time}ms.", sw.ElapsedMilliseconds);
+
+        sr.Close();
+        fs.Close();
+
         movies = movieList.ToArray();
         return true;
     }
 
-    private static bool Write(Movie[] movies) {
-        throw new NotImplementedException("Writing to the file is not currently supported..");
+    private static bool Write(params Movie[] movies) {
+        FileStream fs;
+        StreamWriter sw;
+
+        if(File.Exists(MoviesFull)) {
+            fs = new(MoviesFull, FileMode.Create);
+            sw = new(fs);
+        } else {
+            _logger.Warn("File");
+            return false;
+        }
+
+        foreach(var movie in movies) {
+            sw.WriteLine(movie);
+        }
+
+        sw.Close();
+        fs.Close();
+
+        return true;
     }
+
+    private static bool WriteAppend(params Movie[] movies) {
+        Movie[] newList;
+
+        if(Read(out Movie[] existing)) {
+            newList = new Movie[existing.Length + movies.Length];
+
+            existing.CopyTo(newList, 0);
+            movies.CopyTo(newList, existing.Length);
+        } else {
+            _logger.Warn("Unable to write-append due to issue in file read.");
+            return false;
+        }
+
+        return Write(newList);
+    }
+
+    private static bool CreateRecords(out Movie[] movies) {
+        List<Movie> movieList = new();
+        int nextId = -1;
+
+        if(Read(out Movie[] curMovies)) {
+            foreach(var movie in curMovies) {
+                nextId = Math.Max(nextId, movie.Id);
+            }
+        } else {
+            _logger.Error("Unable to find a suitable ID for new record due to failed read.");
+            movies = Array.Empty<Movie>();
+            return false;
+        }
+
+        while(true) {
+            List<string> genres = new();
+            
+            Console.Write("Enter the name:\n> ");
+            string name = Console.ReadLine() ?? "";
+
+            while(true) {
+                Console.Write("Enter a genre:\n> ");
+                genres.Add(Console.ReadLine() ?? "");
+                
+                Console.Write("Would you like to add another genre (y/n)\n> ");
+                if(Console.ReadLine()?.ToUpper()[0] != 'Y')
+                    break;
+            }
+            
+            movieList.Add(new(nextId++, name, genres.ToArray()));
+            
+            Console.Write("Would you like to add another movie (y/n)\n> ");
+            if(Console.ReadLine()?.ToUpper()[0] != 'Y')
+                break;
+        }
+
+        movies = movieList.ToArray();
+        return true;
+    } 
 }
